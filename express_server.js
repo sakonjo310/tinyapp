10447f1adedf789509c3ed008884b165c85d7e4a
Satoe Sakonjo <sakonjo310@gmail.com>
2022-11-01 14:09:34 -0700

    Add delete function

--- express_server.js
+++ express_server.js
@@ -46,6 +46,12 @@ app.get("/u/:id", (req, res) => {
     res.redirect(longURL);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+    const id = req.params.id;
+    delete urlDatabase[id];
+    res.redirect("/urls");
+})
+
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
