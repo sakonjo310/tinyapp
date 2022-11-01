9b19a83991f978cd325e875a64e861962c3a4323
Satoe Sakonjo <sakonjo310@gmail.com>
2022-11-01 00:35:35 -0700

    Add POST request and redirect client to the website

--- express_server.js
+++ express_server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,6 +24,13 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+    console.log(req.body);
+    const newId = generateRandomString();
+    urlDatabase[newId] = req.body["longURL"];
+    res.redirect(`/urls/${newId}`);
+})
+
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
@@ -22,6 +39,12 @@ app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
   });
+
+app.get("/u/:id", (req, res) => {
+    const id = req.params.id;
+    const longURL = urlDatabase[id];
+    res.redirect(longURL);
+});
 
 app.get("/", (req, res) => {
     res.send("Hello!");
