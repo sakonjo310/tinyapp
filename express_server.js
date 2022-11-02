54054839480525f557c194d1b09ee3090d0e82b3
Satoe Sakonjo <sakonjo310@gmail.com>
2022-11-01 20:53:47 -0700

    Add logout feature

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require('cookie-parser');
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 function generateRandomString() {
     const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -19,8 +22,16 @@ const urlDatabase = {
     "9sm5xK": "http://www.google.com"
 };
 
+app.post("/login", (req, res) => {
+    res.cookie("username", req.body["username"]);
+    res.redirect("/urls");
+})
+
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase};
+    const templateVars = {
+        username: req.cookies["username"],
+        urls: urlDatabase,
+    };
     res.render("urls_index", templateVars);
 });
 
@@ -31,11 +42,16 @@ app.post("/urls", (req, res) => {
 })
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = { username: req.cookies["username"] };
+    res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    const templateVars = {
+        username: req.cookies["username"],
+        id: req.params.id, 
+        longURL: urlDatabase[req.params.id] 
+    };
     res.render("urls_show", templateVars);
 });
 
@@ -53,6 +69,11 @@ app.get("/u/:id", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
     const id = req.params.id;
     delete urlDatabase[id];
+    res.redirect("/urls");
+})
+
+app.post("/logout", (req, res) => {
+    res.clearCookie("username");
     res.redirect("/urls");
 })
 
